8c35bfda31f35666574696392a5e282f706a7c1e
Luke Hudson <lukeletters@gmail.com>
2018-03-16 20:15:51 +0100

    Added features and options: no ghosts mode, set wall thickness

--- control.lua
+++ control.lua
@@ -1,10 +1,67 @@
+local function find_entity_in_inventory(player, name)
+  local inventories = {
+    player.get_inventory(defines.inventory.player_main),
+    player.get_inventory(defines.inventory.player_quickbar),
+    player.get_inventory(defines.inventory.player_tools),
+    player.get_quickbar()
+  }
 
-local function is_empty_position(player, position)
-  local entities = player.surface.find_entities({position,{position.x+1, position.y+1}})
-  if #entities ~= 0 then
-    log('muro ' .. 'found at position '..serpent.block(position) .. ' ' .. serpent.block(entities))
+  local entity = nil
+
+  for _,inventory in ipairs(inventories) do
+    entity = inventory.find_item_stack(name)
+    if entity then
+      return entity
+    end
   end
-  return #entities == 0
+  return nil
+end
+
+local function set_player_cursor_stack(player, entity_name)
+  local stack = find_entity_in_inventory(player, entity_name)
+  if stack ~= nil then
+    -- log('found walls ' .. serpent.line(stack))
+    player.cursor_stack.swap_stack(stack)
+    return true
+  end
+  return false
+end
+
+local function place_wall(player, position)
+  local stack = find_entity_in_inventory(player, 'stone-wall')
+
+  -- if stack == nil then
+  --   log('Didnt find stone-wall in inventory')
+  -- end
+
+  -- log('stone-wall count is ' .. stack.count)
+
+  if not player.surface.can_place_entity({
+    name='stone-wall',
+    position=position,
+    force=player.force,
+    build_check_type=defines.build_check_type.ghost_place
+  }) then
+    -- log("muro: Skipping position where place is not allowed"..serpent.block(position))
+    return
+  end
+
+  local entity_name = 'stone-wall'
+  if not player.cheat_mode and (stack == nil or stack.count < 1) then
+    entity_name = 'entity-ghost'
+  else
+    if stack and stack.count > 0 then
+      stack.count = stack.count - 1
+    end
+  end
+
+  player.surface.create_entity{name=entity_name,
+    inner_name='stone-wall',
+    expires=false,
+    position=position,
+    force=player.force,
+    type='wall'
+  }
 end
 
 local function place_wall_ghost(player, position)
@@ -14,7 +71,7 @@ local function place_wall_ghost(player, position)
     force=player.force,
     build_check_type=defines.build_check_type.ghost_place
   }) then
-    log("muro: Skipping position where place is not allowed"..serpent.block(position))
+    -- log("muro: Skipping position where place is not allowed"..serpent.block(position))
     return
   end
   player.surface.create_entity{name="entity-ghost",
@@ -26,25 +83,44 @@ local function place_wall_ghost(player, position)
   }
 end
 
+local placer = place_wall_ghost
+
+local function init_cheat_mode(player)
+  local setting = settings.get_player_settings(player)["muro-wall-builder-cheat"]
+  -- log('init_cheat_mode, setting is ' .. serpent.line(setting))
+  if setting ~= nil and setting.value == true then
+    placer = place_wall
+  else
+    placer = place_wall_ghost
+  end
+end
+
 -- place a line horizontally of wall ghosts
 local function place_wall_line(player, area, thickness)
   if not thickness then thickness = 1; end
-  for x = area.left_top.x, area.right_bottom.x do
-    place_wall_ghost(player, {x=x, y=area.left_top.y})
+  for i = 0, thickness-1 do
+    for x = area.left_top.x, area.right_bottom.x do
+      placer(player, {x=x, y=area.left_top.y + i})
+    end
   end
 end
 
 local function place_wall_sides(player, area, thickness)
   if not thickness then thickness = 1; end
+  -- handle middle (only sides)
+  for y = area.left_top.y+thickness, area.right_bottom.y-thickness do
+    for i = 0, thickness-1 do
+      placer(player, {x=area.left_top.x+i, y=y})
+      placer(player, {x=area.right_bottom.x-i, y=y})
+    end
+  end
+
 end
 
 local function build_walls(player, area, thickness)
   if not thickness then thickness = 1; end
+  init_cheat_mode(player)
   -- player.print('wall area' .. serpent.block(area))
-  -- area.left_top.x     = math.floor(area.left_top.x + 0.5)
-  -- area.left_top.y     = math.floor(area.left_top.y + 0.5)
-  -- area.right_bottom.x = math.ceil(area.right_bottom.x)
-  -- area.right_bottom.y = math.ceil(area.right_bottom.y)
   -- log( "selcted_area, floored"..serpent.block(area) );
 
   -- Ensure rectangle is integral number of tiles wide/high
@@ -58,78 +134,71 @@ local function build_walls(player, area, thickness)
     return
   end
   if width <= 0 then
-    width = 1
+    width = thickness
   end
   if height <= 0 then
-    height = 1
+    height = thickness
   end
-
 
   -- player.print('muro WxH: ' .. width .. 'x' .. height)
 
   -- handle top line (full line)
-  place_wall_line(player, area)
+  place_wall_line(player, area, thickness)
 
-  if height > 1 then
-    -- handle middle (only sides)
-    for y = area.left_top.y+1, area.right_bottom.y-1 do
-      place_wall_ghost(player, {x=area.left_top.x, y=y})
-      place_wall_ghost(player, {x=area.right_bottom.x, y=y})
-    end
+  place_wall_sides(player, area, thickness)
 
-    -- handle bottom line (full line)
-    area.left_top.y = area.right_bottom.y;
-    place_wall_line(player, area)
-  end
-end
-
-local function find_entity_in_inventory(player, name)
-  local main_inventory = player.get_inventory(defines.inventory.player_main)
-  local quickbar       = player.get_inventory(defines.inventory.player_quickbar)
-  local tools          = player.get_inventory(defines.inventory.player_tools)
-  local entity         = main_inventory.find_item_stack(name)
-
-  if entity == nil then
-    entity = quickbar.find_item_stack(name)
-  end
-  if entity == nil then
-    quickbar = player.get_quickbar()
-    entity   = tools.find_item_stack(name)
-  end
-  if entity == nil then
-    entity = quickbar.find_item_stack(name)
-  end
-  return entity
+  -- handle bottom line (full line)
+  area.left_top.y = area.right_bottom.y - (thickness-1);
+  place_wall_line(player, area, thickness)
 end
 
 local function find_planner(player)
   return find_entity_in_inventory(player, 'muro-wall-builder')
 end
 
-local function on_selected_area(event)
-  log( "selcted_area..."..serpent.block(event) );
+local function on_selected_area(event, thickness)
+  -- log( "selected_area..."..serpent.block(event) );
   if event.item ~= "muro-wall-builder" then return end--If its not our wall builder, exit
 
   local player = game.players[event.player_index]
-  -- local config = global["config"][player.name]
 
-  -- log( "config?"..tostring(config))
-  -- if config == nil then return end
-                    -- global.temporary_ignore[entry.from] = true
-                    -- surface.create_entity{name = "flying-text", position = {tile.position.x-1.3,tile.position.y-0.5}, text = {"insufficient-items"}, color = {r=1,g=0.6,b=0.6}}
-
-  build_walls(player, event.area)
-end
-
-local function init_globals()
-  if not global['muro-wall-builder'] then
-    global['muro-wall-builder'] = {}
-  end
+  build_walls(player, event.area, thickness)
 end
 
 script.on_event(defines.events.on_player_selected_area, function(event)
-  init_globals()
-  on_selected_area(event)
+  local thickness = 1
+  local player    = game.players[event.player_index]
+  local setting   = settings.get_player_settings(player)["muro-wall-builder-thickness"]
+  -- log('setting is '.. serpent.line(setting))
+  if setting and setting.value then
+    thickness = setting.value
+  end
+
+  on_selected_area(event, thickness)
+end)
+
+script.on_event(defines.events.on_player_alt_selected_area, function(event)
+  local thickness = 2
+  local player    = game.players[event.player_index]
+  local setting   = settings.get_player_settings(player)["muro-wall-builder-alt-thickness"]
+  -- log('setting is '.. serpent.line(setting))
+  if setting and setting.value then
+    thickness = setting.value
+  end
+
+  on_selected_area(event, thickness)
+end)
+
+script.on_event(defines.events.on_runtime_mod_setting_changed, function(event)
+  -- log('runtime setting changed ' .. serpent.line(event.setting))
+  if event.setting == 'muro-wall-builder-cheat' then
+    if event.setting.value then
+      placer = place_wall
+    else
+      placer = place_wall_ghost
+    end
+  end
+  return true
 end)
 
 
@@ -145,72 +214,18 @@ script.on_event('muro-wall-builder', function(event)
   end
 end)
 
-script.on_event(defines.events.on_player_crafted_item, function(event)
-  local player = game.players[event.player_index]
+-- script.on_event(defines.events.on_player_crafted_item, function(event)
+--   local player = game.players[event.player_index]
 
-  local item   = event.item_stack
-  if item.name then
-    item = item.name
-  end
-
-  local entity = find_entity_in_inventory(player, item)
-
-  if entity then
-    -- player.print('found item ' .. item .. ' now selecting')
-    player.cursor_stack.swap_stack(entity)
-    -- set_crafting_item(nil)
-  else
-    -- player.print("setting global to " .. item)
-    -- set_crafting_item(item)
-  end
-end)
-
--- local function get_crafting_item()
---   if not global['muro-wall-builder'] or global['muro-wall-builder'].craft == nil then
---       return nil
+--   local item   = event.item_stack
+--   if item.name then
+--     item = item.name
 --   end
---   return global['muro-wall-builder'].craft
--- end
 
--- local function find_last_crafted_entity(player)
---     local item = get_crafting_item()
---     if not item then
---       player.print("No global found" .. serpent.block(global['muro-wall-builder']))
---       return
---     end
+--   local entity = find_entity_in_inventory(player, item)
 
---     player.print('seeking last crafted' .. item)
-
---     local  entity = find_entity_in_inventory(player, item)
---     return entity
--- end
-
--- script.on_event(defines.events.on_player_main_inventory_changed, function(event)
---     local player = game.players[event.player_index]
---     local entity = find_last_crafted_entity(player)
-
---     if entity then
---       set_crafting_item(nil)
---       player.cursor_stack.swap_stack(entity)
---     end
--- end)
-
--- script.on_event(defines.events.on_player_quickbar_inventory_changed, function(event)
---     local player = game.players[event.player_index]
---     local entity = find_last_crafted_entity(player)
-
---     if entity then
---       set_crafting_item(nil)
---       player.cursor_stack.swap_stack(entity)
---     end
--- end)
-
-
--- script.on_event(defines.events.on_player_cursor_stack_changed, function(event)
---     local player = game.players[event.player_index]
---     if player.cursor_stack.valid_for_read then
---       if player.cursor_stack.name == 'muro-wall-builder' then
---       else
---       end
---     end
+--   if entity then
+--     -- player.print('found item ' .. item .. ' now selecting')
+--     player.cursor_stack.swap_stack(entity)
+--   end
 -- end)
